3ca63dd530e63cbd7c44829586389c0aa96f554c
Çetin Tekin <cetintekin@Cetin-MacBook-Pro.local>
2019-09-21 17:07:09 +0300

    zemberek ekli netbeans projesi olusturuldu

---
+++ src/task1/Task1.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task1;
+
+//import java.io.IOException;
+import java.util.List;
+import zemberek.morphology.TurkishMorphology;
+import zemberek.morphology.analysis.WordAnalysis;
+import zemberek.tokenization.Token;
+import zemberek.tokenization.TurkishSentenceExtractor;
+import zemberek.tokenization.TurkishTokenizer;
+
+/**
+ *
+ * @author cetintekin
+ */
+public class Task1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /* Cumleden kelime ve paragraftan cumle extractorlari */
+        TurkishSentenceExtractor extractor = TurkishSentenceExtractor.DEFAULT;
+        TurkishTokenizer tokenizer = TurkishTokenizer.DEFAULT;
+        TurkishMorphology morphology = TurkishMorphology.createWithDefaults();
+        
+        
+        /* Ornek paragraf */
+        String input = "Merhaba! Bugün 2. köprü Fsm.'de trafik var mıydı?";   
+        
+        /* Cumleler extract ediliyor */
+        List<String> sentences = extractor.fromParagraph(input);
+        //System.out.println(sentences);
+        
+        /* Elde edilen her cumlenin kelimeleri sirayla extract edilip kokenleriyle birlikte ekrana basiliyor */
+        List<Token> tokens;
+        WordAnalysis results;
+        int sentenceId = 1;
+        
+        for (String sentence : sentences) {
+            System.out.println(sentenceId + ". Cumle Sonuclari: ");
+            tokens = tokenizer.tokenize(sentence);
+            
+            /* Halihazirda tokenize edilmis cumlenin kelime bilgileri ekrana basiliyor */
+            for (Token token : tokens) {
+                results =  morphology.analyze(token.getText());
+                if(!results.getAnalysisResults().isEmpty())
+                    System.out.println(token.getText()+ " - " + results.getAnalysisResults().get(0).getDictionaryItem().primaryPos); // Kelime ve morfolojik kokeni ekrana basiliyor                
+            }
+            sentenceId++;
+        }
+        
+        
+    }
+    
+}
